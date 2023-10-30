657692ffa594bb208b2a24ce139886b010bd58e6
monir18 <monir112728>
2023-10-30 23:24:38 +0600

    Phases of Java

---
+++ JavaCoding/src/ExecutionSteps.java
@@ -0,0 +1,7 @@
+public class ExecutionSteps {
+    public static void main(String[] args){
+        System.out.print("Java Execution Steps");
+        System.out.print("\n--------------------");
+        System.out.print("\n1.Edit \n2.Compile \n3.Load \n4.Verify \n5.Execute");
+    }
+}
