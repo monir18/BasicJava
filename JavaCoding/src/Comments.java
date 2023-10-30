152226f5c15fae59d7a0b394c9e0704bb46f4dd1
monir18 <monir112728>
2023-10-30 14:07:16 +0600

    Java Comments single line and multi line

---
+++ JavaCoding/src/Comments.java
@@ -0,0 +1,12 @@
+public class Comments {
+    //Multi line comments
+    /*
+    Author: Md.Moniruzzaman
+    Program: Comments
+    Date: 30 October 2023
+     */
+    public static void main(String[] args){
+        //Single line comment
+        System.out.print("Hello everybody, Welcome to my java programming class.");
+    }
+}
