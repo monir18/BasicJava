7191c99e1ddbdb18546a53d0ad2dfd4a332cabab
monir18 <monir112728>
2023-10-30 13:52:28 +0600

    Create BioData class

---
+++ JavaCoding/src/BioData.java
@@ -0,0 +1,8 @@
+public class BioData {
+    public static void main(String[] args){
+        System.out.println("Name    : Md.Moniruzzaman");
+        System.out.println("Age     : 29");
+        System.out.println("S/o     : Md.Abdul Aziz");
+        System.out.println("Address : 17/18 Hossain housing society, Mirpur road, Shyamoli, Dhaka-1207.");
+    }
+}
