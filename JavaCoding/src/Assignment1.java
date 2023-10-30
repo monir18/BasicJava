b7f2d7785895cc87787c61d42569da64c0b81aa5
monir18 <monir112728>
2023-10-30 14:12:24 +0600

    Assignment1 done

---
+++ JavaCoding/src/Assignment1.java
@@ -0,0 +1,13 @@
+/*
+Assignment-1: Create a class called Product, create a main method ,
+print the following data in main method.
+ */
+public class Assignment1 {
+    public static void main(String[] args){
+        System.out.println("Id          : 101");
+        System.out.println("Title       : iphone15");
+        System.out.println("Price       : 1895 euros");
+        System.out.println("Description : perfect product with best image quality");
+        System.out.println("Category    : phone");
+    }
+}
