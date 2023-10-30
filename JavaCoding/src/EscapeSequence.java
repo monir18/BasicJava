a91df42a5e935b533a2223e4d7ca396694394ba4
monir18 <monir112728>
2023-10-30 23:19:00 +0600

    Escape Sequence

---
+++ JavaCoding/src/EscapeSequence.java
@@ -0,0 +1,13 @@
+public class EscapeSequence {
+    public static void main(String[] args){
+        /*
+        System.out.print("Id            : 101");
+        System.out.print("Title         : Iphone15");
+        System.out.print("Price         : 1899 euros");
+        System.out.print("Description   : Perfect product with best image quality");
+        System.out.print("Category      : Phone");
+         */
+
+        System.out.print("Id:\t\t\t\t\t101 \nTitle:\t\t\t\tIphone15 \nPrice:\t\t\t\t1899 euros \nDescription: \t\tPerfect product with best image quality \nCategory: \t\t\tPhone");
+    }
+}
