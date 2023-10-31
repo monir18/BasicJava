9b7f42161039a2f6e7e0e6b51cee8920633664a3
monir18 <monir112728>
2023-10-31 17:15:13 +0600

    Java data type and variable

---
+++ JavaCoding/src/DataTypeAndVariable.java
@@ -0,0 +1,21 @@
+public class DataTypeAndVariable {
+    public static void main(String[] args){
+        boolean b;
+        char c = 'a'; //dynamic initialization
+        short s = 100;
+        int i = 888888888;
+        float f = 20.8f;
+        double d = 99;
+        long l = 999999999;
+
+        b=false;
+
+        System.out.println("Boolean b = " +b);
+        System.out.println("Short s = " +s);
+        System.out.println("Char c = " +c);
+        System.out.println("Int i = " +i);
+        System.out.println("Float f = " +f);
+        System.out.println("Double d = " +d);
+        System.out.println("Long l = " +l);
+    }
+}
