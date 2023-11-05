f6b750d7bdebde0dd48ab3ad83220e0d2507622e
Monir <monir.bu18@gmail.com>
2023-11-05 12:52:33 +0600

    Assignment 2 done

---
+++ JavaCoding/src/Product.java
@@ -0,0 +1,23 @@
+/*
+Assignment-2 (Variable and Data type)
+step 1: create a class called Product
+step 2: create a main method
+step 3: declare variables: id, title, price, description, category
+step 4: assign the following data in main method
+101,iphone15,1895 euros,perfect product with best image quality, phone,
+step 5: print the data
+ */
+public class Product {
+    public static void main(String[] args){
+        int id = 101;
+        String title = "iphone15";
+        double price = 1895;
+        String description = "perfect product with best image quality";
+        String category = "phone";
+        System.out.println(id);
+        System.out.println(title);
+        System.out.println(price);
+        System.out.println(description);
+        System.out.println(category);
+    }
+}
