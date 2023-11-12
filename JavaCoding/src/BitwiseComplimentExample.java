b460335fc5f5f4847e30bc4444921ffae53f7005
Monir <monir.bu18@gmail.com>
2023-11-12 14:10:12 +0600

    BitwiseComplimentExample

---
+++ JavaCoding/src/BitwiseComplimentExample.java
@@ -0,0 +1,9 @@
+public class BitwiseComplimentExample {
+    public static void main(String[] args) {
+        int x = 2;
+        // bitwise complement
+        // ~0010 = 1101 = -3
+
+        System.out.println("~x = "+(~x));
+    }
+}
