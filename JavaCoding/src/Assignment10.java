d3413f632e37f5fd900241b42e2c15a183bdbe19
Monir <monir.bu18@gmail.com>
2023-11-13 13:14:17 +0600

    Assignment10

---
+++ JavaCoding/src/Assignment10.java
@@ -0,0 +1,10 @@
+public class Assignment10 {
+    public static void main(String[] args) {
+        int i, sum = 0;
+        for (i = 1; i <= 10; i++){
+            sum = sum + i;
+            System.out.println(sum);
+        }
+        System.out.println("Total summation of 1-10 = "+sum);
+    }
+}
