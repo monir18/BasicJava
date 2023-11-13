aec21a2910af7ad0c8bf830ffe6a59ad5ba15745
Monir <monir.bu18@gmail.com>
2023-11-13 12:53:51 +0600

    while vs do-while loop

---
+++ JavaCoding/src/whilevsdowhileloop.java
@@ -0,0 +1,15 @@
+public class whilevsdowhileloop {
+    public static void main(String[] args) {
+        int x = 6;
+        while (x <= 5){
+            System.out.println(x+" Bangladesh");
+            x++;
+        }
+        System.out.println("While vs do-while");
+        int a = 6;
+        do {
+            System.out.println(a+ " Bangladesh");
+            a++;
+        }while (a <= 5);
+    }
+}
