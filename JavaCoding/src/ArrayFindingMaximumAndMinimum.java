f66c60a07cb9d312b8d22d35007f9d471b4eccf2
Monir <monir.bu18@gmail.com>
2023-11-22 11:51:43 +0600

    Array Finding Maximum And Minimum Number

---
+++ JavaCoding/src/ArrayFindingMaximumAndMinimum.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ArrayFindingMaximumAndMinimum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        double[] number = new double[5];
+        System.out.print("Enter five numbers : ");
+        for (int i = 0; i < number.length; i++){
+            number[i] = scanner.nextDouble();
+        }
+        for (int i = 0; i < number.length; i++){
+            sum = sum+number[i];
+        }
+        double avg = sum/number.length;
+        System.out.print("Sum of array : "+sum);
+        System.out.print("\nAverage is "+avg);
+
+        double max = number[0];
+        for (int i = 1; i < number.length; i++){
+            if (max < number[i]){
+                max = number[i];
+            }
+        }
+        System.out.println("\nMaximum number is "+max);
+
+        double min = number[0];
+        for (int i = 1; i < number.length; i++){
+            if (min > number[i]){
+                min = number[i];
+            }
+        }
+        System.out.println("Minimum number is "+min);
+    }
+}
