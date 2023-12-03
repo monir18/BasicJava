fedcb4233295586cad4a99900daba6a50bc566b8
Monir <monir.bu18@gmail.com>
2023-12-03 13:01:46 +0600

    Introducing Method Inside A class

---
+++ JavaCoding/src/IntroducingMethodInsideAclass.java
@@ -0,0 +1,11 @@
+public class IntroducingMethodInsideAclass {
+    String name, gender, mobileNumber;
+    int age;
+
+    void displayTeacherInformation(){
+        System.out.println("Name = "+name);
+        System.out.println("Gender = "+gender);
+        System.out.println("Age = "+age);
+        System.out.println("Mobile Number = "+mobileNumber);
+    }
+}
--- JavaCoding/src/OOPTest.java
+++ JavaCoding/src/OOPTest.java
@@ -4,12 +4,23 @@ public class OOPTest {
         JavaClassExample teacher;//object declare
         teacher = new JavaClassExample();//object create
         */
+        
+        /*
         JavaClassExample teacher = new JavaClassExample();
         teacher.name = "Moniruzzaman";
         teacher.gender = "Male";
         teacher.phoneNumber = 1775112828;
+
+
         System.out.println("Name : "+teacher.name);
         System.out.println("Gender : "+teacher.gender);
         System.out.println("Mobile : "+teacher.phoneNumber);
+         */
+        IntroducingMethodInsideAclass teacher = new IntroducingMethodInsideAclass();
+        teacher.name = "Moniruzzaman";
+        teacher.gender = "Male";
+        teacher.mobileNumber = "01775112828";
+        teacher.age = 30;
+        teacher.displayTeacherInformation();
     }
 }
