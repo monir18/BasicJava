8f7e3883c64aa999db08f262c18ed2c4fbbc6ed1
Monir <monir.bu18@gmail.com>
2023-12-13 12:57:36 +0600

    Static Variable Example2

---
+++ JavaCoding/src/StaticVariableExample2.java
@@ -0,0 +1,3 @@
+public class StaticVariableExample2 {
+    static String universityName = "Bangladesh University";
+}
---
+++ JavaCoding/src/StaticVariableTest.java
@@ -0,0 +1,6 @@
+public class StaticVariableTest {
+    public static void main(String[] args) {
+        //StaticVariableExample2 staticVariableExample2 = new StaticVariableExample2();
+        System.out.println("University Name = "+StaticVariableExample2.universityName);
+    }
+}
