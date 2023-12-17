ddc51ec058c717b6af3b494c3aa69aa98e1d2d38
Monir <monir.bu18@gmail.com>
2023-12-17 12:36:00 +0600

    MathClassDemo

---
+++ JavaCoding/src/MathClassDemo.java
@@ -0,0 +1,17 @@
+public class MathClassDemo {
+    public static void main(String[] args) {
+        int x = Math.abs(-9);
+        System.out.println(x);
+
+        double a = Math.abs(-9.5);
+        System.out.println(a);
+
+        System.out.println("Square root = "+Math.sqrt(25));
+
+        System.out.println("Power = "+Math.pow(2,3));
+
+        System.out.println("Ceil = "+Math.ceil(9.5));
+
+        System.out.println("Floor = "+Math.floor(9.5));
+    }
+}
