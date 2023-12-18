c88f48ecce7802cfaec0d90ac8febc514e2c6490
Monir <monir.bu18@gmail.com>
2023-12-18 13:48:29 +0600

    Recursion

---
+++ JavaCoding/src/Recursion.java
@@ -0,0 +1,10 @@
+public class Recursion {
+    int fact(int n){
+        if (n == 1) {
+            return 1;
+        }
+        else {
+            return n*fact(n-1);
+        }
+    }
+}
---
+++ JavaCoding/src/RecursionTest.java
@@ -0,0 +1,7 @@
+public class RecursionTest {
+    public static void main(String[] args) {
+        Recursion recursion = new Recursion();
+        int result = recursion.fact(4);
+        System.out.println("Factorial of 4 = "+result);
+    }
+}
